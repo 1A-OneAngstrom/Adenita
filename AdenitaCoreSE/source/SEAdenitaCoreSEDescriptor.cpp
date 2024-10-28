37b836a6ba96b6bc1e77e9f0b9578421e9c3117a
Dmitriy Marin <dmitriy.marin@oneangstrom.com>
2024-10-28 11:10:14 +0100

    Fix crash when generating atoms. Other changes.
    Fix crash in DASBackToTheAtom::FindAtomsPositions

--- AdenitaCoreSE/source/SEAdenitaCoreSEDescriptor.cpp
+++ AdenitaCoreSE/source/SEAdenitaCoreSEDescriptor.cpp
@@ -33,7 +33,7 @@
 
 SB_ELEMENT_DESCRIPTION("Modeling and Visualization of DNA Nanostructures");
 SB_ELEMENT_DOCUMENTATION("Resource/Documentation/doc.html");
-SB_ELEMENT_VERSION_NUMBER("0.19.2");
+SB_ELEMENT_VERSION_NUMBER("0.19.3");
 
 SB_ELEMENT_CLASSES_BEGIN;
 
